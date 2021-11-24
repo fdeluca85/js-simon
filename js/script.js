c66e10a89bcb4d96dc0f5ced3584b6d6b0361b29
francesca <fradeluca85@gmail.com>
2021-11-24 15:22:25 +0100

    array numeri

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
 // alert('ciao')
+
+// Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi. Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+// generatore array numeri random
+
+let arrayRandomNumber = [];
+while(arrayRandomNumber.length < 5){
+    let numeri = Math.floor(Math.random() * 100) + 1;
+    if(arrayRandomNumber.indexOf(numeri) === -1) arrayRandomNumber.push(numeri);
+}
+console.log(arrayRandomNumber);
+
+// alert('Ricorda questi numeri' + arrayRandomNumber)
+
+
+const element = document.getElementById('numeri_random');
+
+element.innerHTML = arrayRandomNumber;
